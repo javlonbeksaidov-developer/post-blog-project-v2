0a51d5226446b2ce882a9855c1f7df3936149bdd
javlonbeksaidov-developer <javlonbeksaidov09@gmail.com>
2026-09-02 20:36:53 +0500

    start project

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB: str
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    SECRET_KEY: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"{self.DB}://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
